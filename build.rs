315cea9f0fba7162c2f2575d8b5958055a417cf1
psykomal <bkomalsai@gmail.com>
2024-04-12 15:14:25 +0530

    change path

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_file = "./contracts/projects/warehouse_events/warehouse_events.proto";
+    let proto_file = "contracts/projects/warehouse_events/warehouse_events.proto";
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     tonic_build::configure()
