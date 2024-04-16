46e974aa05037a6b28078983c8da13436bf42fce
psykomal <bkomalsai@gmail.com>
2024-04-16 19:41:45 +0530

    prod

--- src/auth.rs
+++ src/auth.rs
@@ -60,14 +60,12 @@ pub fn check_auth_grpc(req: Request<()>) -> Result<Request<()>, Status> {
     let grpc_token = env::var("GRPC_AUTH_TOKEN").expect("GRPC_AUTH_TOKEN is required");
     let token = format!("Bearer {}", grpc_token);
 
-    Ok(req)
-
-    // match req
-    //     .metadata()
-    //     .get("authorization")
-    //     .map(|t| t.to_str().unwrap())
-    // {
-    //     Some(t) if token == t => Ok(req),
-    //     _ => Err(Status::unauthenticated("No valid auth token")),
-    // }
+    match req
+        .metadata()
+        .get("authorization")
+        .map(|t| t.to_str().unwrap())
+    {
+        Some(t) if token == t => Ok(req),
+        _ => Err(Status::unauthenticated("No valid auth token")),
+    }
 }
