299f27526459e7d9c5b6063e419671a61db38183
psykomal <bkomalsai@gmail.com>
2024-04-16 12:14:18 +0530

    testing

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,9 @@
 use std::env;
 
 use axum::response::Html;
+use candid::Deserialize;
 use reqwest::Client;
+use serde::Serialize;
 use serde_json::Value;
 use yup_oauth2::{ExternalAccountAuthenticator, ServiceAccountAuthenticator};
 
@@ -74,13 +76,31 @@ impl WarehouseEvents for WarehouseEventsService {
 //     }
 // }
 
-async fn get_access_token() -> String {
-    let google_creds = env::var("GOOGLE_WL_CREDS").expect("GOOGLE_WL_CREDS is required");
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalAccountSecret {
+    /// audience
+    pub audience: String,
+    /// subject_token_type
+    pub subject_token_type: String,
+    /// service_account_impersonation_url
+    pub service_account_impersonation_url: Option<String>,
+    /// token_url
+    pub token_url: String,
+    // TODO: support service_account_impersonation.
+    /// credential_source
+    pub credential_source: CredentialSource,
+    #[serde(rename = "type")]
+    /// key_type
+    pub key_type: String,
+}
 
-    // Load your service account key
+
+async fn get_access_token() -> String {
+    let google_creds_file = env::var("GOOGLE_WL_CREDS").expect("GOOGLE_WL_CREDS is required");
+
     let creds = yup_oauth2::read_external_account_secret(google_creds).await.expect("GOOGLE_WL_CREDS json file");
 
-    let auth = ExternalAccountAuthenticator::builder(creds)
+    let auth = ExternalAccountAuthenticator::builder(creds.into())
         .build()
         .await
         .unwrap();
@@ -110,3 +130,4 @@ async fn stream_to_bigquery(data: Value) -> Result<(), Box<dyn std::error::Error
         false => Err(format!("Failed to stream data - {:?}", response.text().await?).into()),
     }
 }
+
