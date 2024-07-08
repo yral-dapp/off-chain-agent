0cfdef8c7abb63dd292bab3f4f72992cde274368
komal-rs <komalsai@gobazzinga.io>
2024-07-08 12:49:24 +0530

    video embed bqml

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 use serde_with::serde_as;
@@ -13,6 +15,10 @@ pub struct AppConfig {
 
 impl AppConfig {
     pub fn load() -> Result<Self, ConfigError> {
+        // set env var
+        let sa_key_file = env::var("GOOGLE_SA_KEY").expect("GOOGLE_SA_KEY");
+        env::set_var("SERVICE_ACCOUNT_JSON", sa_key_file);
+
         let conf = Config::builder()
             .add_source(File::with_name("config.toml").required(false))
             .add_source(File::with_name(".env").required(false))
