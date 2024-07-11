6ee3f1fa35c87609d37e1682c1f174d7577dee90
komal-rs <komalsai@gobazzinga.io>
2024-07-11 12:10:31 +0530

    test reclaim canister 1

--- src/consts.rs
+++ src/consts.rs
@@ -10,7 +10,7 @@ pub const PLATFORM_ORCHESTRATOR_ID: &str = "74zq4-iqaaa-aaaam-ab53a-cai";
 pub static YRAL_METADATA_URL: Lazy<Url> =
     Lazy::new(|| Url::parse("https://yral-metadata.fly.dev").unwrap());
 
-pub const RECYCLE_THRESHOLD_SECS: u64 = 60 * 10; // TODO: 15 * 24 * 60 * 60; // 15 days
+pub const RECYCLE_THRESHOLD_SECS: u64 = 15 * 24 * 60 * 60; // 15 days
 
 pub const GOOGLE_CHAT_REPORT_SPACE_URL: &str =
     "https://chat.googleapis.com/v1/spaces/AAAA1yDLYO4/messages";
