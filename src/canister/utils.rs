2dece12a157b619a8a66ea12dbd83f4503468090
komal-rs <komalsai@gobazzinga.io>
2024-08-28 17:48:44 +0530

    old canister list

--- src/canister/utils.rs
+++ src/canister/utils.rs
@@ -247,3 +247,84 @@ pub async fn get_user_and_canisters_list(
 
     Ok(user_canister_ids_map)
 }
+
+pub async fn get_canisters_list_all_old(agent: &Agent) -> Result<Vec<Principal>, Error> {
+    // Platform Orchestrator canister id
+    let pf_o10r_canister_id = Principal::from_text(PLATFORM_ORCHESTRATOR_ID).unwrap();
+
+    // Get the list of subnet orchestrator canister ids
+
+    let response = match agent
+        .query(&pf_o10r_canister_id, "get_all_subnet_orchestrators")
+        .with_arg(encode_args(()).unwrap())
+        .call()
+        .await
+    {
+        Ok(response) => response,
+        Err(err) => {
+            println!("Unable to call the method, error: {:?}", err);
+            return Err(Error::new(std::io::ErrorKind::Other, err));
+        }
+    };
+
+    let subnet_o10r_ids = match candid::decode_one(&response) {
+        Ok(result) => {
+            let result: Vec<Principal> = result;
+            result
+        }
+        Err(err) => {
+            println!("Unable to decode the response, error: {:?}", err);
+            return Err(Error::new(std::io::ErrorKind::Other, err));
+        }
+    };
+    println!(
+        "subnet_o10r_ids {:?}",
+        subnet_o10r_ids
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+    );
+
+    let mut canister_ids_list = vec![];
+
+    // Iterate over the subnet orchestrator canister ids
+    for subnet_o10r_canister_id in &subnet_o10r_ids {
+        // Get individual canister list
+        if subnet_o10r_canister_id.to_string() != "rimrc-piaaa-aaaao-aaljq-cai" {
+            continue;
+        }
+
+        let response = match agent
+            .query(subnet_o10r_canister_id, "get_user_canister_list")
+            .with_arg(encode_args(()).unwrap())
+            .call()
+            .await
+        {
+            Ok(response) => response,
+            Err(err) => {
+                println!("Unable to call the method, error: {:?}", err);
+                return Err(Error::new(std::io::ErrorKind::Other, err));
+            }
+        };
+
+        let canister_ids = match candid::decode_one(&response) {
+            Ok(result) => {
+                let result: Vec<Principal> = result;
+                result
+            }
+            Err(err) => {
+                println!("Unable to decode the response, error: {:?}", err);
+                return Err(Error::new(std::io::ErrorKind::Other, err));
+            }
+        };
+        println!(
+            "subnet_o10r_canister_id {} canister_ids_len {:?}",
+            subnet_o10r_canister_id,
+            canister_ids.len()
+        );
+
+        canister_ids_list.extend(canister_ids);
+    }
+
+    Ok(canister_ids_list)
+}
