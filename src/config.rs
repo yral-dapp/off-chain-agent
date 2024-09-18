9efd61747ab62d92e338b3664c57209fb59408f9
komal-rs <komalsai@gobazzinga.io>
2024-09-18 15:32:53 +0530

    firestore

--- src/config.rs
+++ src/config.rs
@@ -17,6 +17,16 @@ impl AppConfig {
         let sa_key_file = env::var("GOOGLE_SA_KEY").expect("GOOGLE_SA_KEY");
         env::set_var("SERVICE_ACCOUNT_JSON", sa_key_file);
 
+        // create a file
+        let mut file = std::fs::File::create("google_service_account.json").expect("create file");
+        // write the content to the file
+        file.write_all(sa_key_file.as_bytes()).expect("write file");
+
+        env::set_var(
+            "GOOGLE_APPLICATION_CREDENTIALS",
+            "google_service_account.json",
+        );
+
         let conf = Config::builder()
             .add_source(File::with_name("config.toml").required(false))
             .add_source(File::with_name(".env").required(false))
