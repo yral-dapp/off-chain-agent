4e2ab05b9c37f5997f86d23d7f8b9dc003c50c0c
harsh-mahajan-yral <harshmahajan@gobazzinga.io>
2025-02-21 10:14:50 +0530

    sperating video frames and dhash calulation

--- src/video_duplicate/dhash.rs
+++ src/video_duplicate/dhash.rs
@@ -1,78 +1,31 @@
-use image::{imageops::FilterType, DynamicImage, GenericImageView, ImageBuffer, Luma};
-use std::error::Error;
-use tokio::process::Command;
+use image::{imageops::FilterType, DynamicImage, GenericImageView};
 
-pub async fn calculate_video_dhash(video_path: &str) -> Result<Vec<u64>, Box<dyn Error>> {
-    let temp_dir = tempfile::tempdir()?;
-    let output_pattern = temp_dir.path().join("frame_%d.jpg");
+const HASH_SIZE: u32 = 8;
+const WIDTH: u32 = HASH_SIZE + 1;
+const HEIGHT: u32 = HASH_SIZE;
 
-    let status = Command::new("ffmpeg")
-        .args([
-            "-i", video_path,
-            "-vf", "select='not(mod(n,${total_frames}/5))',setpts=N/FRAME_RATE/TB",
-            "-frames:v", "5",
-            "-y",
-            output_pattern.to_str().unwrap(),
-        ])
-        .status()
-        .await?;
+pub struct DHash;
 
-    if !status.success() {
-        return Err("Failed to extract frames".into());
-    }
-
-    let mut hashes = Vec::new();
-    for i in 1..=5 {
-        let frame_path = temp_dir.path().join(format!("frame_{}.jpg", i));
-        let img = image::open(&frame_path)?;
-        let dhash = calculate_dhash(&img);
-        hashes.push(dhash);
-    }
-
-    Ok(hashes)
-}
-
-fn calculate_dhash(img: &DynamicImage) -> u64 {
-    let resized = img.resize_exact(9, 8, FilterType::Lanczos3);
-    let gray = resized.to_luma8();
-
-    let mut hash = 0u64;
-    let mut bit_pos = 0;
+impl DHash {
+    fn calculate_dhash(img: &DynamicImage) -> u64 {
+        let resized = img.resize_exact(9, 8, FilterType::Lanczos3);
+        let gray = resized.to_luma8();
     
-    for y in 0..8 {
-        for x in 0..8 {
-            let left = gray.get_pixel(x, y)[0] as u32;
-            let right = gray.get_pixel(x + 1, y)[0] as u32;
-            
-            if left > right {
-                hash |= 1 << bit_pos;
-            }
-            bit_pos += 1;
-        }
-    }
-    
-    hash
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use image::{ImageBuffer, Luma};
-
-    #[test]
-    fn test_dhash_calculation() {
-        let mut img = ImageBuffer::new(9, 8);
+        let mut hash = 0u64;
+        let mut bit_pos = 0;
         
         for y in 0..8 {
-            for x in 0..9 {
-                let value = if (x + y) % 2 == 0 { 255 } else { 0 };
-                img.put_pixel(x, y, Luma([value]));
+            for x in 0..8 {
+                let left = gray.get_pixel(x, y)[0] as u32;
+                let right = gray.get_pixel(x + 1, y)[0] as u32;
+                
+                if left > right {
+                    hash |= 1 << bit_pos;
+                }
+                bit_pos += 1;
             }
         }
         
-        let dynamic_img = DynamicImage::ImageLuma8(img);
-        let hash = calculate_dhash(&dynamic_img);
-        
-        assert_ne!(hash, 0);
+        hash
     }
 }
