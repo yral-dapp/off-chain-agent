a0215348f3b9d7e8d4c59eb1251fa781bc833114
tushar-saxena-yral <tushar@gobazzinga.io>
2025-04-04 00:36:55 +0530

    chore: prep for review

--- src/consts.rs
+++ src/consts.rs
@@ -22,9 +22,10 @@ pub const CLOUDFLARE_ACCOUNT_ID: &str = "a209c523d2d9646cc56227dbe6ce3ede";
 
 pub const ICP_LEDGER_CANISTER_ID: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 
-// TODO: change back to https://icp-off-chain-agent.fly.dev/
+// TODO: make this an environment variable that is set to point to pr deployment
+// url in prs and to prod url when deploying to main
 pub static OFF_CHAIN_AGENT_URL: Lazy<Url> =
-    Lazy::new(|| Url::parse("https://pr-174-yral-dapp-off-chain-agent.fly.dev/").unwrap());
+    Lazy::new(|| Url::parse("https://icp-off-chain-agent.fly.dev/").unwrap());
 
 pub const NSFW_SERVER_URL: &str = "https://prod-yral-nsfw-classification.fly.dev:443";
 
--- src/events/event/storj.rs
+++ src/events/event/storj.rs
@@ -2,29 +2,32 @@ use std::sync::Arc;
 
 use axum::{extract::State, Json};
 
-use crate::{app_state::AppState, AppError};
+use crate::{
+    app_state::AppState,
+    consts::{STORJ_INTERFACE_TOKEN, STORJ_INTERFACE_URL},
+    AppError,
+};
 
 pub async fn storj_ingest(
-    State(state): State<Arc<AppState>>,
     Json(payload): Json<storj_interface::duplicate::Args>,
 ) -> Result<(), AppError> {
-    // let client = reqwest::Client::new();
-    // client
-    //     .post(
-    //         STORJ_INTERFACE_URL
-    //             .join("/duplicate")
-    //             .expect("url to be valid"),
-    //     )
-    //     .json(&payload)
-    //     .bearer_auth(STORJ_INTERFACE_TOKEN.as_str())
-    //     .send()
-    //     .await?;
-    //
-    log::info!("test succesful");
+    let client = reqwest::Client::new();
+    client
+        .post(
+            STORJ_INTERFACE_URL
+                .join("/duplicate")
+                .expect("url to be valid"),
+        )
+        .json(&payload)
+        .bearer_auth(STORJ_INTERFACE_TOKEN.as_str())
+        .send()
+        .await?;
 
     Ok(())
 }
 
+/// for the purpose of backfilling, can be removed once there are no more items
+/// to be filled
 pub async fn enqueue_storj_backfill_item(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<storj_interface::duplicate::Args>,
